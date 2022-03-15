af0e969556e5b2feaf2729097feb257609bf92d2
marbellys.campos <teamare2>
2022-03-15 07:56:46 -0400

    atrapar excepciones de forma consecutivas

--- excepcionesII.py
+++ excepcionesII.py
@@ -1,7 +1,16 @@
+#Ing. Marbellys Campos
+
 def divide():
-    a = float(input('Introduce el primer valor:'))
-    b = float(input('Introduce el segundo valor:'))
-    print('el calculo es:',a/b)
-    
-    
+    try:
+        a = float(input('Introduce el primer valor:'))
+        b = float(input('Introduce el segundo valor:'))
+        print('el calculo es:' + str(a/b))
+    except ZeroDivisionError:
+        print('Error... division por 0')
+     except ValueError:
+        print('Valor no valido')
+    except:
+        print('Ocurrió un Error')
+    print('Programa finalizado')
+
 divide()
